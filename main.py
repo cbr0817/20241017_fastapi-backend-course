514ec2fe7d2237256ffdd2ad3ef0869556670a89
cbr0817 <chen940817@gmail.com>
2024-10-24 16:08:21 +0800

    20241024 main.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
-
+from pydantic import BaseModel
 app = FastAPI()
 
+class Item(BaseModel):
+    name: str
+    price: float
+    description: str = None
 
 @app.get("/")
 def read_root():
@@ -10,3 +14,8 @@ def read_root():
 @app.get("/nice")
 def read_root():
     return "666，我超級帥，脊椎"
+
+@app.post("/items/")
+def create_item(item: Item):
+    print(f"Received item: {item}")
+    return{"message": "Item received", "item": item}
